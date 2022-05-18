485b2acf0f6d91f2c554040a7729052f31a1871e
Guillaume <guillaume@oss.ventures>
2022-05-18 14:48:02 +0200

    Add .prettierc

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: false,
+  trailingComma: "all",
+  arrowParens: "always",
+  spaceBeforeFunctionParen: false,
+  semi: false,
+  tabWidth: 2,
+  printWidth: 120,
+}
